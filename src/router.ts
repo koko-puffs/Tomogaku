e1ef57dcaad2fc0b06594adca84c05a3bb4f1467
Silas Lund <112756812+KeroSilas@users.noreply.github.com>
2024-11-14 07:57:11 +0100

    Fix

--- src/router.ts
+++ src/router.ts
@@ -29,7 +29,7 @@ const router = createRouter({
   ]
 })
 
-router.beforeEach(async (to, from, next) => {
+router.beforeEach(async (to, _from, next) => {
   const authStore = useAuthStore()
   
   // Wait for auth check to complete
